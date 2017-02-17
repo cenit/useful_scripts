c4c90f38b043474b2b7806d25b6f6c1b4b0f497a
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-02-17 11:28:31 +0000

    add special instructions

--- docs/prepare_docs.sh
+++ docs/prepare_docs.sh
@@ -2,7 +2,7 @@
 
 cd ~/Codice
 
-repo_doxygenate=("calib_lib", "math_lib", "rdp_lib", "vbb_datastore", "vbb_meta", "vbb_texa", "metabox", "texabox")
+repo_doxygenate=("calib_lib" "math_lib" "rdp_lib" "vbb_datastore" "vbb_meta" "vbb_texa" "texabox")
 for repo in ${repo_doxygenate[*]} ; do
   if [ -d $repo ]; then
     cd $repo
@@ -11,8 +11,16 @@ for repo in ${repo_doxygenate[*]} ; do
   fi
 done
 
+#special treatment for metabox (due to SVN folder structure)
+repo="metabox"
+if [ -d $repo ]; then
+  cd $repo/trunk
+  pandoc --from=markdown README.md .pandoc.yml -o ../${repo}.pdf
+  cd ../..
+fi
 
-repo_soloreadme=("data_tools", "datalogger", "distance_stats", "get_nominatim_info", "inertial_tools", "json_distance", "json_packer", "json_to_html", "json_to_kml", "kml_to_json", "libgnssconv", "list_to_json", "log_parser", "Quaternioni", "ubx_to_json")
+
+repo_soloreadme=("data_tools" "datalogger" "distance_stats" "get_nominatim_info" "inertial_tools" "json_distance" "json_packer" "json_to_html" "json_to_kml" "kml_to_json" "libgnssconv" "list_to_json" "log_parser" "Quaternioni" "ubx_to_json")
 for repo in ${repo_soloreadme[*]} ; do
   if [ -d $repo ]; then
     cd $repo
