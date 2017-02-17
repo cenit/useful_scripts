e8c42313d07b373ddbab02fa4465cffa5cbfae68
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-02-17 09:04:03 +0000

    add a docs script (requires pandoc, texlive and the templates repo installed

---
+++ docs/prepare_docs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd ~/Codice
+
+repo_doxygenate=("calib_lib", "math_lib", "rdp_lib", "vbb_datastore", "vbb_meta", "vbb_texa", "metabox", "texabox")
+for repo in ${repo_doxygenate[*]} ; do
+  if [ -d $repo ]; then
+    cd $repo
+    pandoc --from=markdown README.md .pandoc.yml -o ../${repo}.pdf
+    cd ..
+  fi
+done
+
+
+repo_soloreadme=("data_tools", "datalogger", "distance_stats", "get_nominatim_info", "inertial_tools", "json_distance", "json_packer", "json_to_html", "json_to_kml", "kml_to_json", "libgnssconv", "list_to_json", "log_parser", "Quaternioni", "ubx_to_json")
+for repo in ${repo_soloreadme[*]} ; do
+  if [ -d $repo ]; then
+    cd $repo
+    pandoc --from=markdown README.md -o ../${repo}.pdf
+    cd ..
+  fi
+done
+
