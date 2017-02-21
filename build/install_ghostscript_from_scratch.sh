b7d937fa85db7dc2c80a9b75502fab2061d8ba56
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-02-21 17:24:47 +0000

    add gs

---
+++ build/install_ghostscript_from_scratch.sh
@@ -0,0 +1,14 @@
+# make sure to add ${BUILD_FOLDER}/bin to PATH
+
+BUILD_FOLDER=/shared/software/project/aladyn/ghostscript/
+
+rm -rf ${BUILD_FOLDER}
+mkdir -p ${BUILD_FOLDER}
+
+wget https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs920/ghostscript-9.20.tar.gz
+tar zxvf ghostscript-9.20.tar.gz
+cd ghostscript-9.20
+./configure --prefix="${BUILD_FOLDER}"
+make
+make install
+
