4837cb16ab518cb5657c598d5297da6e8c842847
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-11-30 11:37:22 +0100

    update fork updater

--- forks/update_forks.sh
+++ forks/update_forks.sh
@@ -1,203 +1,26 @@
 #!/bin/bash
 
-echo Smilei
-cd Smilei/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
+REPO=json
+  if [ -d "$REPO" ]; then
+    echo $REPO
+    cd $REPO/
+    git fetch upstream
+    git checkout develop
+    git merge upstream/develop
+    git push
+    cd ..
+  fi
 
-echo PIConGPU
-cd picongpu/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo psc
-cd psc/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo fbpic
-cd fbpic/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-
-echo gnuplot-colorbrewer
-cd gnuplot-colorbrewer/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo VTKFortran 
-cd VTKFortran/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo ramses 
-cd ramses/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo epoch 
-cd epoch/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo EPOCH_manuals 
-cd EPOCH_manuals/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo QGIS 
-cd QGIS/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo json 
-cd json/
-git fetch upstream
-git checkout develop
-git merge upstream/develop
-git push
-cd ..
-
-echo jsoncons 
-cd jsoncons/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo Nominatim 
-cd Nominatim/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo mosquitto 
-cd mosquitto/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo geopy 
-cd geopy/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo pyUblox 
-cd pyUblox/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo Catch 
-cd Catch/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo RTKLIB 
-cd RTKLIB/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo numerical-methods-fortran  
-cd numerical-methods-fortran/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo densityClust
-cd densityClust
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo tclap
-cd tclap/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo community_bash_style_guide
-cd community_bash_style_guide/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo ALaDyn_cenit
-cd ALaDyn_cenit/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo Propaga_cenit
-cd Propaga_cenit/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
-echo Stats
-cd Stats/
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push
-cd ..
-
+REPOSITORIES=(Smilei PIConGPU psc fbpic gnuplot-colorbrewer VTKFortran ramses epoch EPOCH_manuals QGIS jsoncons Nominatim mosquitto geopy pyUblox Catch RTKLIB numerical-methods-fortran densityClust tclap community_bash_style_guide)
+for REPO in ${REPOSITORIES[*]}
+do
+  if [ -d "$REPO" ]; then
+    echo $REPO
+    cd $REPO/
+    git fetch upstream
+    git checkout master
+    git merge upstream/master
+    git push
+    cd ..
+  fi
+done
