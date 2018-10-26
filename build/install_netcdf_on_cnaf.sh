5e9fd5f40ff2ae801b3b65042361ea0f5bd8658f
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-10-26 12:20:21 +0200

    add netcdf-fortran lib

--- build/install_netcdf_on_cnaf.sh
+++ build/install_netcdf_on_cnaf.sh
@@ -9,6 +9,9 @@ export CXX=$(which g++)
 export FC=$(which gfortran)
 export F90=$(which gfortran)
 
+export H5DIR=/shared/software/project/aladyn/hdf5/hdf5-1.8.20/
+export NCDIR=/shared/software/project/aladyn/netcdf/netcdf-4.4.1.1/
+
 rm -rf /shared/software/project/aladyn/netcdf
 
 cd /shared/software/project/aladyn/
@@ -17,16 +20,28 @@ cd netcdf/
 mkdir build
 cd build/
 wget https://github.com/Unidata/netcdf-c/archive/v4.4.1.1.tar.gz
-mv v4.4.1.1.tar.gz netcdf-4.4.1.1.tar.gz
-tar zxvf netcdf-4.4.1.1.tar.gz 
-cd netcdf-c-4.4.1.1/
-H5DIR=/shared/software/project/aladyn/hdf5/hdf5-1.8.20/
-NCDIR=/shared/software/project/aladyn/netcdf/
+mv v4.4.1.1.tar.gz netcdf-c-4.4.1.1.tar.gz
+wget https://github.com/Unidata/netcdf-fortran/archive/v4.4.4.tar.gz
+mv v4.4.4.tar.gz netcdf-fortran-4.4.4.tar.gz
+tar zxvf netcdf-c-4.4.1.1.tar.gz 
+tar zxvf netcdf-fortran-4.4.4.tar.gz
 
+cd netcdf-c-4.4.1.1/
 mkdir build
 cd build
-cmake .. -D"CMAKE_INSTALL_PREFIX=${NCDIR}" -D"HDF5_DIR=${H5DIR}" 
+cmake .. -D"CMAKE_INSTALL_PREFIX=${NCDIR}" -D"HDF5_ROOT=${H5DIR}" -DCMAKE_BUILD_TYPE=Release 
 cmake --build . --target install
 cd ..
-rm -rf build
+cd ..
+
+cd netcdf-fortran-4.4.4
+mkdir build
+cd build
+cmake .. -D"CMAKE_INSTALL_PREFIX=${NCDIR}" -D"HDF5_ROOT=${H5DIR}" -DCMAKE_BUILD_TYPE=Release
+cmake --build . --target install
+cd ..
+cd ..
+
+rm -rf netcdf-c-4.4.1.1
+rm -rf netcdf-fortran-4.4.4
 
