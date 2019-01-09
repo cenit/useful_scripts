c1ad43bd28cea64bc1451536d1caefb5f795f29a
Stefano Sinigardi <stesinigardi@hotmail.com>
2019-01-09 09:52:04 +0100

    fix wrong variable expansion, add better log info

--- forks/update_forks.sh
+++ forks/update_forks.sh
@@ -3,52 +3,60 @@
 REPOSITORIES_CLONED=(Smilei fbpic PIConGPU psc gnuplot-colorbrewer VTKFortran ramses epoch EPOCH_manuals websocketpp curlpp QGIS jsoncons Nominatim mosquitto geopy pyUblox Catch RTKLIB numerical-methods-fortran densityClust voc-dpm Dictionaries)
 for REPO in ${REPOSITORIES_CLONED[*]}
 do
-  if [ -d "$REPO" ]; then
-    echo $REPO
-    cd $REPO/
+  if [ -d "${REPO}" ]; then
+    echo "Updating --- ${REPO}"
+    cd ${REPO}/
     git fetch -p origin
     git push --mirror
     cd ..
+  else
+    echo "Unable to find --- $REPO --- skipping"
   fi
 done
 
 REPOSITORIES_CLONED_CENIT=(Multitarget-tracker gdl Detectron community_bash_style_guide azure-batch-apps-blender L1--L2---Computation-of-satellite-and-receiver-position)
 for REPO in ${REPOSITORIES_CLONED_CENIT[*]}
 do
-  if [ -d "$REPO_cenit" ]; then
-    echo $REPO
-    cd $REPO_cenit/
+  if [ -d "${REPO}_cenit" ]; then
+    echo "Updating --- ${REPO}_cenit"
+    cd ${REPO}_cenit/
     git fetch -p origin
     git push --mirror
     cd ..
+  else
+    echo "Unable to find --- ${REPO}_cenit --- skipping"
   fi
 done
 
 REPOSITORIES_CUSTOMIZED=math
 for REPO in ${REPOSITORIES_CUSTOMIZED[*]}
 do
-  if [ -d "$REPO_cenit" ]; then
-    echo $REPO_cenit
-    cd $REPO_cenit/
+  if [ -d "${REPO}_cenit" ]; then
+    echo "Updating --- ${REPO}_cenit"
+    cd ${REPO}_cenit/
     git checkout develop
     git fetch upstream
     git merge upstream/develop
     git push
     cd ..
+  else
+    echo "Unable to find --- ${REPO}_cenit --- skipping"
   fi
 done
 
 REPOSITORIES_CUSTOMIZED=(vcpkg Propaga ALaDyn tools-piccante matconvnet vlfeat sudoku-detector voc-release4.01 libsvm Data-Analysis nocs ProteinPipe crowd-counting cmake)
 for REPO in ${REPOSITORIES_CUSTOMIZED[*]}
 do
-  if [ -d "$REPO_cenit" ]; then
-    echo $REPO_cenit
-    cd $REPO_cenit/
+  if [ -d "${REPO}_cenit" ]; then
+    echo "Updating --- ${REPO}_cenit"
+    cd ${REPO}_cenit/
     git checkout master
     git fetch upstream
     git merge upstream/master
     git push
     cd ..
+  else
+    echo "Unable to find --- ${REPO}_cenit --- skipping"
   fi
 done
 
@@ -56,7 +64,7 @@ REPOSITORIES_CUSTOMIZED=(darknet)
 for REPO in ${REPOSITORIES_CUSTOMIZED[*]}
 do
   if [ -d "${REPO}_cenit" ]; then
-    echo ${REPO}_cenit
+    echo "Updating --- ${REPO}_cenit"
     cd ${REPO}_cenit/
     git checkout master
     git fetch upstream_pj
@@ -68,5 +76,7 @@ do
     git push
     git checkout master
     cd ..
+  else
+    echo "Unable to find --- ${REPO}_cenit --- skipping"
   fi
 done
