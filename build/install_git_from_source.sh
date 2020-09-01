5d29ab263f00e803c6bcaeab97315f295c59904b
Stefano Sinigardi <stesinigardi@hotmail.com>
2020-09-01 12:10:18 +0200

    add a note about prerequisites

--- build/install_git_from_source.sh
+++ build/install_git_from_source.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+#sudo apt-get install asciidoc
+#sudo apt-get install docbook2x
+#sudo apt-get install libcurl4-openssl-dev  #libcurl4-gnutls-dev has problems with proxies
+#sudo apt-get install dh-autoreconf libexpat1-dev gettext libz-dev libssl-dev
+#sudo apt-get install xmlto
+
 GIT_VERSION=2.28.0
 wget https://github.com/git/git/archive/v${GIT_VERSION}.tar.gz --no-check-certificate
 tar zxvf v${GIT_VERSION}.tar.gz
